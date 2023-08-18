ac37c2c477bb658f55cc5962363dc7ab5926c58e
Artem Achkasov <artemkryt1800@gmail.com>
2023-08-18 20:59:48 +0300

    fixes + use outside click

--- uiKit/ToolTip/index.tsx
+++ uiKit/ToolTip/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { FC, useState } from "react";
+import { useOutsideClick } from "@/utils/hooks/useOnClickOutside";
+import { FC, useRef, useState } from "react";
 import { IconType } from "react-icons";
 import { TbInfoOctagon } from "react-icons/tb";
 import { ToolTipText, ToolTipWrap } from "./style";
@@ -14,8 +15,16 @@ interface PropsType {
 export const ToolTip: FC<PropsType> = ({ text, icon: Icon, size = 25 }) => {
   const [isVisible, setIsVisible] = useState(false);
 
+  const handleClose = () => {
+    setIsVisible(false);
+  };
+
+  const tooltipRef = useRef(null);
+
+  useOutsideClick(tooltipRef, handleClose, isVisible);
+
   return (
-    <ToolTipWrap onClick={() => setIsVisible((prev) => !prev)}>
+    <ToolTipWrap onClick={() => setIsVisible((prev) => !prev)} ref={tooltipRef}>
       {!!isVisible && <ToolTipText>{text}</ToolTipText>}
       {Icon ? <Icon /> : <TbInfoOctagon size={size} />}
     </ToolTipWrap>
---
+++ utils/hooks/useOnClickOutside.ts
@@ -0,0 +1,30 @@
+import { RefObject, useEffect } from "react";
+import { useLatest } from "./useLatest";
+
+type ClickHandler = () => void;
+
+export function useOutsideClick(
+  elementRef: RefObject<HTMLElement>,
+  handler: ClickHandler,
+  attached = true
+) {
+  const latestHandler = useLatest(handler);
+
+  useEffect(() => {
+    if (!attached) return;
+
+    const handleClick = (e: MouseEvent) => {
+      if (!elementRef.current) return;
+
+      if (!elementRef.current.contains(e.target as Node)) {
+        latestHandler.current();
+      }
+    };
+
+    document.addEventListener("click", handleClick);
+
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  }, [elementRef, latestHandler, attached]);
+}
