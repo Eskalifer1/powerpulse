acaee47ca2f6aa6d1a45d719d544a6b6a3321684
Artem Achkasov <artemkryt1800@gmail.com>
2023-09-09 17:19:52 +0300

    verification tag

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,4 +1,5 @@
 import HomePageScreen from "@/Screens/HomePage";
+import { Metadata } from "next";
 import { getTranslator } from "next-intl/server";
 
 export async function generateMetadata({
@@ -11,7 +12,10 @@ export async function generateMetadata({
   return {
     title: t("title"),
     description: t("description"),
-  };
+    verification: {
+      google: "lz4tG-Z5Tt8AwVDMTwh35dgNLhI_gIoEYgPgFWpUMX8"
+    }
+  } as Metadata;
 }
 
 export default function Home() {
